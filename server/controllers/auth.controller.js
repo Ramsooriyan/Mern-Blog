4b8616d0540ff386ce76f3f98c87808e1cae8f71
ramsooriyan <ram160102@gmail.com>
2024-07-03 10:13:55 +0530

    update header component

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -67,10 +67,11 @@ export const google = async(req, res, next) => {
       }).json(rest);
     }else{
       const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
-      const hashPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username: name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
         email,
+        password: hashedPassword,
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
